5599634fd745918b64bf6936572700a6e3223b42
niels.rustenburg <niels.rustenburg@gmail.com>
2021-12-07 22:39:38 +0100

    Awwwww bb did I tell you, F# is my best friend

--- advent2021/advent2021/Program.fs
+++ advent2021/advent2021/Program.fs
@@ -3,6 +3,7 @@ open Solve1
 open Solve2
 open Solve3
 open Solve4
+open Solve5
 open System
 
 //printfn "%d" d1p1
@@ -11,5 +12,7 @@ open System
 //printfn "%d" d2p2
 //printfn "%d" d3p1
 //printfn "%d" d3p2
-printfn "%d" d4p1
-printfn "%d" d4p2
+//printfn "%d" d4p1
+//printfn "%d" d4p2
+printfn "%d" d5p1
+printfn "%d" d5p2
---
+++ advent2021/advent2021/Solve5.fs
@@ -0,0 +1,27 @@
+﻿module Solve5
+open System
+open Input
+
+type Coordinate =
+    {
+        X : int
+        Y : int
+    }
+
+type VentLine = 
+    {
+        Origin : Coordinate
+        Destination : Coordinate
+    }
+
+let parseVentCoords (coords: string) : Coordinate = coords.Split ',' |> fun c -> {X = Int32.Parse c[0]; Y = Int32.Parse c[1]}
+let parseVentLine (line: string) : VentLine = line.Split " -> " |> fun x -> { Origin = parseVentCoords x[0]; Destination = parseVentCoords x[1] }
+let parseAllVentLines = readDayInput 5 |> Seq.map parseVentLine
+let filterOutDiagonals (ventLines : seq<VentLine>) = Seq.filter (fun (l:VentLine) -> l.Destination.X = l.Origin.X || l.Destination.Y = l.Origin.Y ) ventLines
+let getDiff (line : VentLine) : Coordinate = {X = line.Destination.X - line.Origin.X; Y = line.Destination.Y - line.Origin.Y}
+let getStepFromDiff (diff: Coordinate) = if diff.X = 0 then {X = diff.X / (abs diff.Y); Y = diff.Y / (abs diff.Y) } else {X = diff.X / (abs diff.X); Y = diff.Y / (abs diff.X) }
+let rec GetPointsBetween (start: Coordinate) (stop:Coordinate) (step: Coordinate) = if start = stop then Seq.singleton stop else seq {yield start; yield! (GetPointsBetween {X = start.X+step.X; Y = start.Y+step.Y} stop step)} 
+let getAllPointsInLine (line : VentLine) : seq<Coordinate> = getDiff line |> fun diff -> GetPointsBetween line.Origin line.Destination (getStepFromDiff diff)
+
+let d5p1 = parseAllVentLines |> filterOutDiagonals |> Seq.fold (fun acc l -> Seq.append acc (getAllPointsInLine l)) Seq.empty |> Seq.countBy (fun c -> c) |> Seq.filter (fun kvp -> snd kvp > 1) |> Seq.length
+let d5p2 = parseAllVentLines |> Seq.fold (fun acc l -> Seq.append acc (getAllPointsInLine l)) Seq.empty |> Seq.countBy (fun c -> c) |> Seq.filter (fun kvp -> snd kvp > 1) |> Seq.length
