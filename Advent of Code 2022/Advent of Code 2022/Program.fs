981fb4b9647ab5cd9f6db78cf771b6a69231c8a7
niels.rustenburg <niels.rustenburg@gmail.com>
2022-12-03 11:08:41 +0100

    d3p1

--- Advent of Code 2022/Advent of Code 2022/Program.fs
+++ Advent of Code 2022/Advent of Code 2022/Program.fs
@@ -1,11 +1,17 @@
 ﻿open Input
 open Solve1
 open Solve2
+open Solve3
 
 //printfn "%A" d1p1
 //printfn "%A" d1p1
 //printfn "%A" d2p1
-printfn "%A" d2p2
 //printfn "%A" d2p2
+//printfn "%A" d2p2
+
+printfn "%A" d3p1
+printfn "%A" d3p2
+
+
 //printfn "%A" (Seq.toList d1p1)
 //printfn "%d" d1p2
---
+++ Advent of Code 2022/Advent of Code 2022/Solve3.fs
@@ -0,0 +1,22 @@
+﻿module Solve3
+
+open Input
+open FParsec
+open System.Linq
+
+let halfString (s:string) = (s[..(s.Length/2)-1],s[(s.Length/2)..])
+let sortString (s:string) = Seq.sort s |> Seq.map string |> String.concat ""
+let commonElement (a:string,b:string) = (Set.intersect (Set.ofSeq a) (Set.ofSeq b)) 
+                                        |> Set.toSeq
+                                        |> Seq.head
+
+let priorities = Seq.append (seq {'a' .. 'z' }) (seq{'A' .. 'Z'}) |> Seq.toList
+let prioVal (x:char) = (List.findIndex (fun y -> x = y) priorities) + 1
+
+let rucksack = restOfLine false .>> newline |>> halfString
+let valueRucksack = rucksack |>> commonElement |>> prioVal
+let p1Rucksacks = many valueRucksack
+
+
+let d3p1 = readDayInputAsSingleString 3 |> runwrap p1Rucksacks |> List.sum
+let d3p2 = readDayInputAsSingleString 3 |> fun _ -> "3"
