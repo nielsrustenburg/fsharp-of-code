e5173e97bb3bd42cfa6ed44dcc69e428231affac
niels.rustenburg <niels.rustenburg@gmail.com>
2022-12-03 11:21:09 +0100

    d3p2

--- Advent of Code 2022/Advent of Code 2022/Solve3.fs
+++ Advent of Code 2022/Advent of Code 2022/Solve3.fs
@@ -2,7 +2,6 @@
 
 open Input
 open FParsec
-open System.Linq
 
 let halfString (s:string) = (s[..(s.Length/2)-1],s[(s.Length/2)..])
 let sortString (s:string) = Seq.sort s |> Seq.map string |> String.concat ""
@@ -13,10 +12,19 @@ let commonElement (a:string,b:string) = (Set.intersect (Set.ofSeq a) (Set.ofSeq 
 let priorities = Seq.append (seq {'a' .. 'z' }) (seq{'A' .. 'Z'}) |> Seq.toList
 let prioVal (x:char) = (List.findIndex (fun y -> x = y) priorities) + 1
 
-let rucksack = restOfLine false .>> newline |>> halfString
-let valueRucksack = rucksack |>> commonElement |>> prioVal
+let p1rucksack = restOfLine false .>> newline |>> halfString
+let valueRucksack = p1rucksack |>> commonElement |>> prioVal
 let p1Rucksacks = many valueRucksack
 
-
 let d3p1 = readDayInputAsSingleString 3 |> runwrap p1Rucksacks |> List.sum
-let d3p2 = readDayInputAsSingleString 3 |> fun _ -> "3"
+
+
+let findBadge (a:string,b:string,c:string) = Set.intersectMany [Set.ofSeq a; Set.ofSeq b; Set.ofSeq c]
+                                                |> Set.toSeq
+                                                |> Seq.head
+
+let p2threeElves = tuple3 (restOfLine true) (restOfLine true) (restOfLine false .>> newline)
+let p2ValueOfThree = p2threeElves |>> findBadge |>> prioVal
+let p2ValueOfAll = many p2ValueOfThree
+
+let d3p2 = readDayInputAsSingleString 3 |> runwrap p2ValueOfAll |> List.sum
