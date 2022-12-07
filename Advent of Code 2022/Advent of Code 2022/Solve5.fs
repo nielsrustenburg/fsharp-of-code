0a766007ad32ecaea2ebb4dc79f8b6e61a0d5800
niels.rustenburg <niels.rustenburg@gmail.com>
2022-12-07 23:30:44 +0100

    d5p1

--- Advent of Code 2022/Advent of Code 2022/Solve5.fs
+++ Advent of Code 2022/Advent of Code 2022/Solve5.fs
@@ -16,7 +16,8 @@ let rec addLayerToMap (layer:char list) (labels: char list) map:Map<char, char l
     | ([lastBox], [lastLabel]) -> addToMap lastBox lastLabel map
     | (h1::t1, hl::tl) -> addLayerToMap t1 tl (addToMap h1 hl map)
     | _ -> failwith "empty or unequal lists"
-    
+
+// TODO: in theory input could start with an empty stack, in which case we still want an empty list added to the map
 let rec mapToStacks (map: Map<char, char list>) (stacks: char list list, labels: char list) = 
     match stacks with
     | [x] -> addLayerToMap x labels map
@@ -28,9 +29,24 @@ let pempty = pstring "   " >>% '0'
 let pline = sepBy1 (pbox <|> pempty) (pchar ' ') 
 let pstacks = sepEndBy pline newline
 let plabel = between (pchar ' ') (pchar ' ') anyChar
-let plabels = sepBy plabel (pchar ' ')
+let plabels = sepBy plabel (pchar ' ') .>> newline
 let parrangement = pstacks .>>. plabels |>> mapToStacks Map.empty
+let pinstruction = tuple3 (pstring "move " >>. pint32) (pstring " from " >>. anyChar) (pstring " to " >>. anyChar)
+let pinstructions = sepEndBy pinstruction newline
+let pwholeinput = (parrangement .>> newline) .>>. pinstructions
+let rec applyInstruction (stacks: Map<char, char list>) (amount: int32, from: char, towards: char) = 
+    match (amount, Map.find from stacks, Map.find towards stacks) with
+    | (0, _,_) -> stacks
+    | (_, hfrom::tfrom, tow) -> applyInstruction (Map.add from tfrom (Map.add towards (hfrom::tow) stacks)) (amount-1, from, towards)
+    | _ -> failwith "you donkey"
 
-let d5p1 = readDayInputAsSingleString 5 |> runwrap parrangement
+let rec applyAllInstructions (instructions: (int * char * char) list) (stacks: Map<char, char list>) = 
+    match instructions with
+    | [ins] -> applyInstruction stacks ins
+    | h::t  -> (applyInstruction stacks h) |> applyAllInstructions t
+    | _ -> failwith "you buffoon"
 
-let d5p2 = readDayInputAsSingleString 5 |> fun _ -> 5
+let getTopBoxesAsString (stacks: Map<char, char list>) = Map.fold (fun s k v -> (k,List.head v)::s) [] stacks |> List.sortBy (fun (k,v) -> k) |> List.map (fun (k,v) -> v) |> Array.ofList |> System.String
+
+let d5p1 = readDayInputAsSingleString 5 |> runwrap pwholeinput |> fun (x,y) -> applyAllInstructions y x |> getTopBoxesAsString
+let d5p2 = "soon"
